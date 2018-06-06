a3281a7b4a4c15b7f7978174a4be4bd015a903ee
Ron Craig <ron.craig@comcast.net>
2018-06-06 10:09:12 -0700

    add liveTime function to update time every second

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-let greeting = React.createElement('h1', {}, 'Hello, world!');
-let clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-let app = React.createElement('div', {}, greeting, clock);
+function liveTime() {
+  let greeting = React.createElement('h1', {}, 'Hello world!');
+  let clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+  let app = React.createElement('div', {}, greeting, clock);
 
-ReactDOM.render(
-  app,
-  document.getElementById('react-app-root')
-);
+  ReactDOM.render(
+    app,
+    document.getElementById('react-app-root')
+  );
+}
+
+setInterval(liveTime, 1000);
